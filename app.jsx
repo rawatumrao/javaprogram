e7c7d31ec5eb7da30960b751f0bf55118c801529
rawatumrao <rawatumrao@gmail.com>
2024-11-30 00:46:46 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import BitmovinPlayer from './BitmovinPlayer';
+
+function App() {
+  return (
+    <div className="App">
+      <BitmovinPlayer />
+    </div>
+  );
+}
+
+export default App;
